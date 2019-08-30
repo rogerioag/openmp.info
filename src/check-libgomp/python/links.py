2c6181199aa606482bb1f4b78a87b711f5e5cbfc
venturini <danielventurini021@gmail.com>
2019-08-29 22:41:04 -0300

    Add info worker and update regex

--- src/check-libgomp/python/links.py
+++ src/check-libgomp/python/links.py
@@ -29,6 +29,7 @@ def is_gcc_release(file):
 
 # get all gcc releases/tags from gcc.gnu.org
 def get_gcc_releases():
+    print('get gcc releases...', end='', flush=True)
     gcc_releases = []
     tags = svn.remote.RemoteClient(tags_url).list()
     for file in tags:
@@ -37,6 +38,7 @@ def get_gcc_releases():
             gcc_releases.append(file)
 
     save_releases(gcc_releases)
+    print('OK')
 
 # from the line 'svn://gcc.gnu.org/svn/gcc/tags/gcc_4_0_0_release/libgomp/libgomp.map\n'
 # remove the 'svn:' and replace by 'https:' to do a request and remove the \n
@@ -49,15 +51,23 @@ def request(url):
 
 # check the existents mirror of repository release
 def check_existence():
+    print('checking libgomp...')
     file_fail_output   = open(path + releases_file_name_output.format('_fail'), 'w')
     file_sucess_output = open(path + releases_file_name_output.format('_sucess'), 'w')
 
     for line in open(path + releases_file_name_input).readlines():
-        print(line, end='')
-        file_sucess_output.write(line) if request(format_line(line)) else file_fail_output.write(line)
+        # just print, eg, 'gcc_4_2_0_release/...OK'
+        print('    ' + re.search('gcc_\d_\d(_\d)*(_pre[^/]*)*', line).group(0) + '...', end='', flush=True)
+        if request(format_line(line)):
+            file_sucess_output.write(line)
+            print('OK')
+        else:
+            print('FALSE')
+            file_fail_output.write(line)
 
     file_fail_output.close()
     file_sucess_output.close()
+    print('checking libgomp...OK')
 
 # download all paths libgomps files from each release into workspace folder
 # if force=True, then all libgomps files for each release will be replaced
@@ -65,17 +75,22 @@ def download_libgomps(force=True):
     try: os.mkdir(workspace)
     except FileExistsError: pass
 
+    print('download libgomps...')
     file_sucess_input = open(path + releases_file_name_output.format('_sucess'))
     # create and export all libgomp paths from all releases
     for url in file_sucess_input.readlines():
         url = url.replace('libgomp.map\n', '')
-        # get the 'gcc_4_2_0_release/', eg
-        release = re.search('gcc_\d_\d_\d_release/', url).group(0)
+        # get the 'gcc_4_2_0_release/', eg, and prereleases too
+        release = re.search('gcc_\d_\d(_\d)*(_pre[^/]*\/)*(_release\/)*', url).group(0)
         #os.mkdir(workspace + release)
 
+        print('    ' + re.search('gcc_\d_\d(_\d)*(_pre[^/]*)*', url).group(0) + '...', end='', flush=True)
         svn.remote.RemoteClient(url).export(workspace + release, force=force)
+        print('OK')
+
+    print('download libgomps...OK')
 
 if __name__ == '__main__':
-    get_gcc_releases()
-    check_existence()
+    #get_gcc_releases()
+    #check_existence()
     download_libgomps()
