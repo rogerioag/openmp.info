18b74e9c294c6cf63c4c7ddb846a2722cf79e74f
venturini <danielventurini021@gmail.com>
2019-09-04 21:35:44 -0300

    Add ppd model

---
+++ src/simple-omp-hook/scripts/ctags-test/model/pos_ppd.c
@@ -0,0 +1,8 @@
+//\\ all lines in this file will be on END of the prepostdef.h
+//\\ lines starts with //\\ will be ignored
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PREPOSTDEF_H */
---
+++ src/simple-omp-hook/scripts/ctags-test/model/pre_ppd.c
@@ -0,0 +1,15 @@
+//\\ all lines in this file will be at BEGIN of the prepostdef.h
+//\\ lines starts with //\\ will be ignored
+#ifndef PREPOSTDEF_H
+#define PREPOSTDEF_H
+
+#define PREPOSTDEF 1
+
+typedef unsigned long long gomp_ull;
+typedef long TYPE;
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
